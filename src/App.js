8fb363c6f48d13baa44036f5c7668b2f905c061f
Christian Tukunda <christiantukundastocklin@gmail.com>
2024-06-05 18:52:43 +0100

    Description des modifications

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // Assurez-vous d'importer le fichier CSS d'AOS
+import { Analytics } from "@vercel/analytics/react";
 // import logo from './logo.svg';
 // import logo from '../public/Fusion-Inc-logo.jpg';
 import logo from './Fusion-Inc-logo.jpg';
