1d486c55ad973f4b15a87a0dad870ce7e8c1489c
Christian Tukunda <christiantukundastocklin@gmail.com>
2024-06-06 01:12:40 +0100

    Description des modifications

--- src/component/aboutComponent.js
+++ src/component/aboutComponent.js
@@ -8,10 +8,10 @@ function aboutComponent() {
             <div className="center">
                 <div className="aboutBlockJ6">
                     <div className="detail">
-                        <div className="nameCompany">A propos</div>
+                        <div className="nameCompany">Fusion-Photo</div>
                         <h2 className="title">A propos de nous</h2>
                         <div className="text">
-                            Qui sommes nous ?..et quels services proposons nous?
+                        Qui sommes-nous ? Et quels services proposons-nous ?
 
                             <br/>
                             <button>Rendez_vous</button>
@@ -20,11 +20,9 @@ function aboutComponent() {
                     </div>
                     
                     <div className="description">
-                        <p>Nous sommes une agence de photographie <strong>Fusion-Photo</strong> basé à Kinshasa, RDC. Nos photographes professionnels sont dédiés à capturer la beauté et l'émotion de chaque instant, transformant vos souvenirs en trésors intemporels.</p>
+                        <p>Nous sommes une <strong>agence de photographie professionnelle</strong> <strong>Fusion-Photo</strong> basée à <strong>Kinshasa, RDC</strong>. Nos <strong>photographes professionnels</strong> sont dédiés à capturer la beauté et l'émotion de chaque instant, transformant vos souvenirs en trésors intemporels.</p>
                         <br/>
-                        <p>Nous sommes fiers de vous offrir une qualité photo qui dépasse toutes les attentes.
-                            Grâce à notre maîtrise technique et à notre passion pour l'art visuel, chaque cliché devient une œuvre d'art.
-                        </p>
+                        <p>Nous sommes fiers de vous offrir une <strong>qualité photo inégalée</strong> qui dépasse toutes les attentes. Grâce à notre <strong>maîtrise technique</strong> et à notre passion pour l'<strong>art visuel</strong>, chaque cliché devient une œuvre d'art.</p>
                     </div>
 
                 </div>  
